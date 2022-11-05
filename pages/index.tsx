bf6ea43b3599abbfc8859abe4d3d8b9ba958b54d
ukyoda <black12172001@hotmail.co.jp>
2022-11-05 17:10:59 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,21 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>五目並べ</h1>
       <Gomoku width={19} height={19} />
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <p>
+            &copy; Produced by
+            <a href='https://twitter.com/ukyoda' target='_blank' rel='noopener noreferrer' >
+              @ukyoda
+            </a>
+          </p>
+          <p>
+            <a href='https://github.com/ukyoda/gomoku-narabe'  target='_blank' rel='noopener noreferrer'>
+              Github
+            </a>
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
